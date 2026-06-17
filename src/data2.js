5896d0646b2a5baa3fe36179e59aea4d190ef715
Ola <ponikowska.books@gmail.com>
2026-06-17 17:28:17 +0200

    tematy

--- src/data2.js
+++ src/data2.js
@@ -40,5 +40,47 @@ export const tematy = {
       "nazwa": "Diagnostyka, NAT i zaawansowane IP", 
       "tasks": [11, 18, 29, 30, 39] 
     }
+  ],
+  "2013": [
+    { 
+      "id": "1", 
+      "nazwa": "Podstawy adresacji i podsieci (Obliczenia)", 
+      "tasks": [12, 17, 21, 34, 38] 
+    },
+    { 
+      "id": "2", 
+      "nazwa": "Mechanizmy routingu i architektura sieci", 
+      "tasks": [1, 3, 24, 39, 40] 
+    },
+    { 
+      "id": "3", 
+      "nazwa": "Protokół TCP: Połączenia i kontrola", 
+      "tasks": [4, 10, 13, 23, 36] 
+    },
+    { 
+      "id": "4", 
+      "nazwa": "Warstwa transportowa, UDP i Gniazda", 
+      "tasks": [5, 16, 18, 30, 35] 
+    },
+    { 
+      "id": "5", 
+      "nazwa": "Warstwa łącza, transmisja i Ethernet", 
+      "tasks": [6, 14, 25, 27, 31] 
+    },
+    { 
+      "id": "6", 
+      "nazwa": "Usługi sieciowe i Warstwa Aplikacji", 
+      "tasks": [2, 15, 20, 22, 28] 
+    },
+    { 
+      "id": "7", 
+      "nazwa": "Bezpieczeństwo, Szyfrowanie i Ataki", 
+      "tasks": [19, 26, 32, 33, 37] 
+    },
+    { 
+      "id": "8", 
+      "nazwa": "NAT, diagnostyka i mechanizmy sieciowe", 
+      "tasks": [7, 8, 9, 11, 29] 
+    }
   ]
 };
